b797fc7fe114a94d721635985377de1c0302c927
prosittes <jonatas7cristian@gmail.com>
2025-09-22 17:53:06 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 export const supabase = createClient(
-  https://gyommnaimtmabbhrxhwp.supabase.co
-  'SUA-ANON-KEY' eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5b21tbmFpbXRtYWJiaHJ4aHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1NTM5MjIsImV4cCI6MjA3NDEyOTkyMn0.QUd74zCRE5uF03_GzVWwgd3pmuA4xlu-YTWz8jk7_BE
+  'https://gyommnaimmbabbnfxwp.supabase.co', // Project URL
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOi...' // sua anon public key completa
 )
