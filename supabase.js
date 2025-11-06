bf5fcfc6a891073fd8f65e9dc20958c81cb21a76
prosittes <jonatas7cristian@gmail.com>
2025-11-06 00:11:46 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-export const supabase = createClient(
-  'https://gyommnaimmbabbnfxwp.supabase.co', // Project URL
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOi...' // sua anon public key completa
-)
